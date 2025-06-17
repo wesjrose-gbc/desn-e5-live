84ef33622d173906d19b9494a2af49907d0ac36a
Wesley Rose <101625918@georgebrown.ca>
2025-06-17 00:01:27 +0000

    Finished creating automated script and adding puppeeter workflow

---
+++ scripts/main.mjs
@@ -0,0 +1,30 @@
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import lighthouse from 'lighthouse';
+// Or import puppeteer from 'puppeteer-core';
+
+// Launch the browser and open a new blank page
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+const page_url = 'https://wesjrose-gbc.github.io/desn-e5-live/' 
+
+// Navigate the page to a URL.
+await page.goto(page_url);
+
+// Set screen size.
+await page.setViewport({width: 1080, height: 1024});
+await page.waitForNetworkIdle();
+
+await page.screenshot({
+    path: 'auto_screenshot.png'
+});
+
+const options = {output: 'html'};
+const runnerResult = await lighthouse(page_url, options, undefined, page);
+
+// `.report` is the HTML report as a string
+const reportHtml = runnerResult.report;
+fs.writeFileSync('auto_report.html', reportHtml);
+
+await browser.close();
